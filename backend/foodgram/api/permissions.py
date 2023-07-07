f20ebae933c778c49e8ec8f674480caaa5f4c23a
liubov-py <118378080+liubov-py@users.noreply.github.com>
2023-07-07 19:59:46 +0300

    Update permissions.py

--- backend/foodgram/api/permissions.py
+++ backend/foodgram/api/permissions.py
@@ -5,10 +5,8 @@ class IsAdminOrAuthorOrReadOnly(permissions.BasePermission):
     """Изменение только для автора или админа."""
 
     def has_object_permission(self, request, view, obj):
-        return (
-            request.method in permissions.SAFE_METHODS or obj.author ==
-            request.user or request.user.is_staff
-            )
+        return (request.method in permissions.SAFE_METHODS
+                or obj.author == request.user or request.user.is_staff)
 
 
 class IsAuthorOrReadOnly(permissions.BasePermission):
