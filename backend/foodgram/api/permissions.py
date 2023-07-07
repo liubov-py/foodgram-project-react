900943c458441cc87d96285acd8fb6d1ac9ed2a1
Любовь Пузанова <liubovpuzanova@yandex.ru>
2023-07-07 19:48:33 +0300

    Reco implemented

--- backend/foodgram/api/permissions.py
+++ backend/foodgram/api/permissions.py
@@ -5,12 +5,10 @@ class IsAdminOrAuthorOrReadOnly(permissions.BasePermission):
     """Изменение только для автора или админа."""
 
     def has_object_permission(self, request, view, obj):
-        return (request.method in permissions.SAFE_METHODS or
-                obj.author == request.user or
-                request.user.is_staff)
-        # if obj.author == request.user or request.user.is_staff:
-        #     return True
-        # return False
+        return (
+            request.method in permissions.SAFE_METHODS or obj.author ==
+            request.user or request.user.is_staff
+            )
 
 
 class IsAuthorOrReadOnly(permissions.BasePermission):
