c1c628b573805feb3cb942cf2607d45f82e5a7be
Любовь Пузанова <liubovpuzanova@yandex.ru>
2023-07-10 23:33:17 +0300

    All

--- foodgram/foodgram/settings.py
+++ foodgram/foodgram/settings.py
@@ -131,10 +131,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = (BASE_DIR / 'static')
+STATICFILES_DIRS = ((BASE_DIR / 'static/'),)
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = (BASE_DIR / 'media')
+MEDIA_ROOT = ((BASE_DIR, 'media'),)
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
