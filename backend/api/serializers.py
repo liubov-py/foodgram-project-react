699641aec2f42f92b8ba647be516feacd433de41
Любовь Пузанова <liubovpuzanova@yandex.ru>
2023-07-10 23:44:31 +0300

    All

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -99,8 +99,7 @@ class RecipeIngredienReadSerializer(serializers.ModelSerializer):
     id = serializers.ReadOnlyField(source='ingredient.id')
     name = serializers.ReadOnlyField(source='ingredient.name')
     measurement_unit = serializers.ReadOnlyField(
-        source='ingredient.measurement_unit'
-        )
+        source='ingredient.measurement_unit')
 
     class Meta:
         model = RecipeIngredient
