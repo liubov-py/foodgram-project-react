ee01c86ae9bd518b4988407ca4f8c93a46929c20
Любовь Пузанова <liubovpuzanova@yandex.ru>
2023-07-10 16:46:01 +0300

    Main.yml

--- backend/foodgram/api/permissions.py
+++ backend/foodgram/api/permissions.py
@@ -6,8 +6,8 @@ class IsAdminOrAuthorOrReadOnly(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return (
-            request.method in permissions.SAFE_METHODS or obj.author ==
-            request.user or request.user.is_staff
+            request.method in permissions.SAFE_METHODS
+            or obj.author == request.user or request.user.is_staff
             )
 
 
