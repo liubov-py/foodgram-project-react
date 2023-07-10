960eb682129f6b6f1b8d2b1e4b4f2106e06469a7
Любовь Пузанова <liubovpuzanova@yandex.ru>
2023-07-10 19:58:36 +0300

    F

--- backend/run_app.sh
+++ backend/run_app.sh
@@ -1,6 +1,6 @@
-cd foodgram || exit
+cd foodgram || exit;
 python manage.py migrate;
 python manage.py import_csv;
 python manage.py collectstatic --noinput;
-cp -r /app/collected_static/. /backend_static/static/
+cp -r /app/collected_static/. /backend_static/static/;
 gunicorn -b 0:8000 foodgram.wsgi;
