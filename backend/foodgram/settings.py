7352c2c560f8eb7f4a2e122d825f17dba04c9735
Любовь Пузанова <liubovpuzanova@yandex.ru>
2023-07-12 19:51:24 +0300

    Final

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -87,8 +87,8 @@ else:
             'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
             'HOST': os.getenv('DB_HOST', ''),
             'PORT': os.getenv('DB_PORT', 5432)
-            }
-            }
+        }
+    }
 
 # DATABASES = {
 #     'default': {
