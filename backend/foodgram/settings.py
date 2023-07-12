9486150b75cf2f2031bb147f669e580598f8ed33
Любовь Пузанова <liubovpuzanova@yandex.ru>
2023-07-12 19:36:58 +0300

    Final

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -81,14 +81,14 @@ if DEBUG:
 else:
     DATABASES = {
         'default': {
-           'ENGINE': 'django.db.backends.postgresql',
-           'NAME': os.getenv('POSTGRES_DB', 'django'),
-           'USER': os.getenv('POSTGRES_USER', 'django'),
-           'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-           'HOST': os.getenv('DB_HOST', ''),
-           'PORT': os.getenv('DB_PORT', 5432)
-        }
-    }
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('POSTGRES_DB', 'django'),
+            'USER': os.getenv('POSTGRES_USER', 'django'),
+            'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+            'HOST': os.getenv('DB_HOST', ''),
+            'PORT': os.getenv('DB_PORT', 5432)
+            }
+            }
 
 # DATABASES = {
 #     'default': {
